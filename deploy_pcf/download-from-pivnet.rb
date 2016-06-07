88f5e099296e8484e73a0391a52af899c8e949bd
Gaurab Dey <gdey@pivotal.io>
2016-06-07 16:32:35 -0700

    adds download script to shared repo
    [#120221725]
    
    Signed-off-by: Joe Baldwin <jbaldwin@pivotal.io>

---
+++ deploy_pcf/download-from-pivnet.rb
@@ -0,0 +1,127 @@
+#!/usr/bin/env ruby
+
+require 'httparty'
+require 'optparse'
+
+def get_latest_product_version(product_name)
+  url = "#{@pivnet_api}/#{product_name}/releases"
+  product_releases = make_get_request(url).parsed_response
+  product_versions = product_releases['releases'].map {|release| release['version']}
+  product_versions.keep_if { |a| a=~ /^[0-9].[0-9].*$/}
+  product_versions.sort! {|a,b| b <=> a }.first
+end
+
+
+def make_get_request(endpoint)
+  response = HTTParty.get(endpoint,
+              headers: {
+                'Accept' => "application/json",
+                'Content-Type' => "application/json",
+                'Authorization' => "Token #{@pivnet_token}",
+              }
+    )
+  unless response.success?
+    raise "Error making a GET request to #{endpoint}"
+  end
+  response
+end
+
+def make_post_request(endpoint)
+  response = HTTParty.post(endpoint,
+              # :debug_output => $stdout,
+              headers: {
+                'Accept' => "application/json",
+                'Content-Type' => "application/json",
+                'Authorization' => "Token #{@pivnet_token}",
+              }
+    )
+  unless response.success?
+    raise "Error making a POST request to #{endpoint}"
+  end
+  response
+end
+
+def wget_from_pivnet(endpoint, filename, path=nil)
+  if path
+    Dir.mkdir path
+    `wget -O #{path}/#{filename} --post-data="" --header="Authorization: Token #{@pivnet_token}" #{endpoint}`
+  else
+    `wget -O #{filename} --post-data="" --header="Authorization: Token #{@pivnet_token}" #{endpoint}`
+  end
+end
+
+def download(product, version=nil)
+  releases_url = "#{@pivnet_api}/#{product}/releases"
+  product_releases = make_get_request(releases_url).parsed_response
+  if !version.empty?
+    release = product_releases['releases'].select { |release| release['version'] == version }.first
+  else
+    release = product_releases['releases'].first
+  end
+
+  if !release
+    puts "Cannot find #{version} for #{product}. See available versions below:\n"
+    product_releases['releases'].each {|release| puts release['version']}
+    exit
+  end
+  product_files = make_get_request("#{releases_url}/#{release['id']}/product_files").parsed_response
+
+  if product == 'ops-manager'
+    product_file_name = product_files['product_files'].select { |product_files| product_files['name'].include? 'vSphere'}.first['aws_object_key'].split('/').last
+    product_file_id = product_files['product_files'].select { |product_files| product_files['name'].include? 'vSphere'}.first['id']
+  elsif product == 'elastic-runtime'
+    download_object = product_files['product_files'].select {|product| product['name'] == 'PCF Elastic Runtime'}.first
+    product_file_name = download_object['aws_object_key'].split('/').last
+    product_file_id = download_object['id']
+  end
+
+  download_link = "#{releases_url}/#{release['id']}/product_files/#{product_file_id}/download"
+  make_post_request("#{releases_url}/#{release['id']}/eula_acceptance")
+  wget_from_pivnet(download_link, product_file_name)
+end
+
+raise "Please set the env variable 'PIVNET_TOKEN' to be your network.pivotal.io token" if ENV['PIVNET_TOKEN'].nil?
+@pivnet_api = 'https://network.pivotal.io/api/v2/products'
+@pivnet_token = ENV.fetch('PIVNET_TOKEN')
+
+options = {
+  :ops_manager => get_latest_product_version('ops-manager'),
+  :elastic_runtime => get_latest_product_version('elastic-runtime'),
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: --ops-manager <om-version> --elastic-runtime <ert-version>"
+  opts.on('-o', '--ops-manager [OM]') do |ops_manager|
+    options[:ops_manager] = ops_manager
+    options[:ops_manager] ||= get_latest_product_version('ops-manager')
+  end
+  opts.on('-e', '--elastic-runtime [ERT]') do |elastic_runtime|
+    options[:elastic_runtime] = elastic_runtime
+    options[:elastic_runtime] ||= get_latest_product_version('elastic-runtime')
+  end
+  opts.on('-h', '--help [ERT]') do |help|
+    options[:help] = help
+    puts opts
+    options[:help] ||= 'help'
+  end
+end.parse!
+
+
+if options[:help]
+  ops_manager_url = "#{@pivnet_api}/ops-manager/releases"
+  ops_manager_releases = make_get_request(ops_manager_url).parsed_response
+  puts "Available Ops Manager versions:"
+  ops_manager_versions = ops_manager_releases['releases'].map {|release| release['version']}
+  p ops_manager_versions.sort! {|a,b| b <=> a }
+
+  elastic_runtime_url = "#{@pivnet_api}/elastic-runtime/releases"
+  elastic_runtime_releases = make_get_request(elastic_runtime_url).parsed_response
+  puts "\nAvailable Elastic Runtime versions:"
+  elastic_runtime_versions = elastic_runtime_releases['releases'].map {|release| release['version']}
+  p elastic_runtime_versions.sort! {|a,b| b <=> a }
+  exit
+end
+
+
+options.each {|product,version|puts "Downloading: #{product} - #{version}\n" }
+download('ops-manager', options[:ops_manager]) if options[:ops_manager]
+download('elastic-runtime', options[:elastic_runtime]) if options[:elastic_runtime]
