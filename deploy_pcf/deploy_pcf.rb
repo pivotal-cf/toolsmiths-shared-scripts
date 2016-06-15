a2a9e0929a06b486c302a2109622bc030eef3269
Brenda Chan <brchan@pivotal.io>
2016-06-15 14:21:47 -0400

    Adds coloring to shell output, fix fetching stemcell version by using `unzip`

--- deploy_pcf/deploy_pcf.rb
+++ deploy_pcf/deploy_pcf.rb
@@ -4,6 +4,52 @@ require 'optparse'
 include Process
 require 'pry'
 
+def green;          "\e[32m#{self}\e[0m" end
+def red;            "\e[31m#{self}\e[0m" end
+def cyan;           "\e[36m#{self}\e[0m" end
+
+def attempt(cmd)
+  puts "*".cyan * 72
+  puts "running #{cmd}...".cyan
+  puts Time.now.cyan
+  puts "*".cyan * 72
+
+  pid = Process.spawn("time " + cmd)
+  trap('SIGINT') { Process.kill('HUP', pid) }
+  pid, status = waitpid2(pid)
+  exit_status = status.exitstatus
+
+  case exit_status
+    when 0
+      puts "SUCCESS: #{cmd} completed".green
+    when 1
+      puts "FAILED:  #{cmd}".red
+      exit 1
+    else
+      puts "Ooh, got a weird exit status: #{exit_status}".red
+  end
+end
+
+def download_stemcell(path_to_product_tarball,iaas)
+  puts "Finding stemcell version for #{path_to_product_tarball}".cyan
+  `unzip #{path_to_product_tarball} metadata/cf.yml && cat metadata/cf.yml | grep -A3 stemcell | grep version | grep -oE "[0-9\.]+" > stemcell_version`
+  version = `cat stemcell_version`.chomp!
+  puts "Downloading stemcell version #{version} for #{iaas}"
+
+  case iaas
+  when 'vsphere'
+    `wget --content-disposition https://bosh.io/d/stemcells/bosh-vsphere-esxi-ubuntu-trusty-go_agent?v=#{version}`
+  when 'aws'
+    `wget --content-disposition https://bosh.io/d/stemcells/bosh-aws-xen-hvm-ubuntu-trusty-go_agent?v=#{version}`
+  when 'openstack'
+    `wget --content-disposition https://bosh.io/d/stemcells/bosh-openstack-kvm-ubuntu-trusty-go_agent-raw?v=#{version}`
+  else
+    raise "cannot find iaas named #{iaas}".red
+  end
+  file_name = `ls bosh-stemcell-#{version}-#{iaas}*`.split("\n").first
+  File.expand_path(file_name)
+end
+
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: deploy_pcf.rb [options]"
@@ -53,43 +99,6 @@ OptionParser.new do |opts|
   @opts = opts
 end.parse!
 
-def attempt(cmd)
-  puts "*" * 72
-  puts "running #{cmd}..."
-  puts Time.now
-  puts "*" * 72
-
-  pid = Process.spawn("time " + cmd)
-  trap('SIGINT') { Process.kill('HUP', pid) }
-  pid, status = waitpid2(pid)
-  exit_status = status.exitstatus
-
-  case exit_status
-    when 0
-      puts "SUCCESS: #{cmd} completed"
-    when 1
-      puts "FAILED:  #{cmd}"
-      exit 1
-    else
-      puts "Ooh, got a weird exit status: #{exit_status}"
-  end
-end
-
-def download_stemcell(path_to_product_tarball,iaas)
-  version = `tar -xvf #{path_to_product_tarball} metadata/cf.yml && cat metadata/cf.yml | grep -A3 stemcell | grep version | grep -oE "[0-9\.]+"`.chomp!
-  case iaas
-  when 'vsphere'
-    `wget --content-disposition https://bosh.io/d/stemcells/bosh-vsphere-esxi-ubuntu-trusty-go_agent?v=#{version}`
-  when 'aws'
-    `wget --content-disposition https://bosh.io/d/stemcells/bosh-aws-xen-hvm-ubuntu-trusty-go_agent?v=#{version}`
-  when 'openstack'
-    `wget --content-disposition https://bosh.io/d/stemcells/bosh-openstack-kvm-ubuntu-trusty-go_agent-raw?v=#{version}`
-  else
-    raise "cannot find iaas named #{iaas}"
-  end
-  file_name = `ls bosh-stemcell-#{version}-#{iaas}*`.split("\n").first
-  File.expand_path(file_name)
-end
 
 if options.empty?
   puts @opts
@@ -106,6 +115,7 @@ if options[:ops_manager_version] == 'latest'
   download_pivnet_file = File.expand_path(File.dirname(__FILE__)) + "/download-from-pivnet.rb"
   options[:ops_manager_version] = `#{download_pivnet_file} --print-latest ops-manager`
 end
+
 if options[:elastic_runtime_version] == 'latest'
   download_pivnet_file = File.expand_path(File.dirname(__FILE__)) + "/download-from-pivnet.rb"
   options[:elastic_runtime_version] = `#{download_pivnet_file} --print-latest elastic-runtime`
@@ -114,14 +124,10 @@ end
 options[:ops_manager_version] = options[:ops_manager_version].match('[0-9]+\.[0-9]').to_s if options[:ops_manager_version]
 options[:elastic_runtime_version] = options[:elastic_runtime_version].match('[0-9]+\.[0-9]').to_s if options[:elastic_runtime_version]
 
-if options[:headless]
-  # xvfb server can only run one at a time - use -a flag to automatically find a free server number
-  xvfb = "xvfb-run -a "
-end
+# xvfb server can only run one at a time - use -a flag to automatically find a free server number
+xvfb = "xvfb-run -a " if options[:headless]
 
-if options[:stemcell].nil? && options[:elastic_runtime]
-  options[:stemcell] = download_stemcell(options[:elastic_runtime],options[:iaas])
-end
+options[:stemcell] = download_stemcell(options[:elastic_runtime],options[:iaas]) if options[:stemcell].nil? && options[:elastic_runtime]
 
 cmds = [
   "bundle exec rake opsmgr:destroy[#{environment}]",
@@ -161,7 +167,6 @@ if options[:interactive]
     exit
   end
 end
-
 
 if options[:dry_run]
   puts cmds.join(" &&\\\n")
