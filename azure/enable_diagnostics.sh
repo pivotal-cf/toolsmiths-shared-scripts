b54b86c747ffe79e2dd796032f1882824d102fc8
Gaurab Dey <gdey@pivotal.io>
2016-07-08 10:06:07 -0700

    adds scripts to enable usage in each VM (auzre env)
    [#124869971]
    
    Signed-off-by: Neil Sant Gat <negat@microsoft.com>

---
+++ azure/enable_diagnostics.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# assumes you have a file called PrivateConfig.json with your storage account credentials (documentation here: https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-linux-classic-diagnostic-extension/)
+
+if [ "$#" -lt 2 ]
+then
+    echo "enables the diagnostics extension on all VMs in a resource group."
+    echo "assumes you have a file called PrivateConfig.json with your storage account credentials (documentation here: https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-linux-classic-diagnostic-extension/)"
+    echo ""
+    echo "command: ./enable_diagnostics.sh YOUR_RG_NAME PATH_TO_YOUR_PRIVATE_CONFIG_FILE"
+    echo ""
+    echo "The above command runs in parallel; if you want it to run serially, run it as:"
+    echo ""
+    echo "command ./enable_diagnostics.sh YOUR_RG_NAME PATH_TO_YOUR_PRIVATE_CONFIG_FILE serial"
+
+    exit 0
+fi
+
+
+resource_group_name=$1
+path_to_private_config_file=$2
+serial=$3
+
+vm_list=$(azure vm list $resource_group_name | grep $resource_group_name | awk '{print $3}')
+for vm in $vm_list
+do
+    if [ "$serial" = "serial" ]
+    then
+	azure vm extension set -g $resource_group_name -n LinuxDiagnostic -p Microsoft.OSTCExtensions -o 2.3 --private-config-path $path_to_private_config_file -m $vm
+    else
+	azure vm extension set -g $resource_group_name -n LinuxDiagnostic -p Microsoft.OSTCExtensions -o 2.3 --private-config-path $path_to_private_config_file -m $vm &
+    fi
+done
