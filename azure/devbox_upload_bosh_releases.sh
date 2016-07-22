2ce84bde565c681742b9ea5cd1505788d451744e
Brenda Chan <brchan@pivotal.io>
2016-07-22 11:22:21 -0700

    Skip bosh release/stemcell upload if exists.
    Signed-off-by: Derwei Chan <dchan@pivotal.io>

--- azure/devbox_upload_bosh_releases.sh
+++ azure/devbox_upload_bosh_releases.sh
@@ -4,9 +4,9 @@ set -x
 bosh target 10.0.0.4
 bosh login admin PASSWORDHERE
 
-bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-mysql-release?v=24
-bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-release?v=231
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=0.333.0
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=36
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=0.1454.0
-bosh upload stemcell https://bosh.io/d/stemcells/bosh-azure-hyperv-ubuntu-trusty-go_agent?v=3232.11
+bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-mysql-release?v=24 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-release?v=231 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=0.333.0 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=36 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=0.1454.0 --skip-if-exists
+bosh upload stemcell https://bosh.io/d/stemcells/bosh-azure-hyperv-ubuntu-trusty-go_agent?v=3232.11 --skip-if-exists
