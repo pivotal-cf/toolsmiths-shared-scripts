f4302f7cfca323d0a3bb1922220d30b7c2b8da03
Mark Stokan <mstokan@pivotal.io>
2016-07-28 12:16:06 -0700

    minor release version number is required.  updated to use the latest.
    Signed-off-by: Joe Baldwin <jbaldwin@pivotal.io>

--- azure/azure.tf
+++ azure/azure.tf
@@ -301,7 +301,7 @@ resource "azurerm_virtual_machine" "devboxvm" {
     storage_image_reference {
         publisher = "Canonical"
         offer = "UbuntuServer"
-        sku = "14.04-LTS"
+        sku = "14.04.4-LTS"
         version = "latest"
     }
 
