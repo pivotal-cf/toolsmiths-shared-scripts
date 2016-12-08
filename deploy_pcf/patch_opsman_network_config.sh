b881f552c839d26c761d43694251beb32acf76db
Todd Ritchie <tritchie+github@pivotal.io>
2016-12-07 17:06:45 -0800

    remve hardcoded env_name from script
    Signed-off-by: Gaurab Dey <gdey@pivotal.io>

--- deploy_pcf/patch_opsman_network_config.sh
+++ deploy_pcf/patch_opsman_network_config.sh
@@ -6,7 +6,7 @@ echo "Getting token ..."
 uaac token owner get opsman $OPSMAN_USERNAME -s '' -p $OPSMAN_PASSWORD
 UAA_ACCESS_TOKEN=$(uaac context | grep access_token | xargs | cut -f2 -d" ")
 echo "Token: ${UAA_ACCESS_TOKEN}"
-BOSH_GUID=$(curl -k "https://pcf.undercity.cf-app.com/api/v0/staged/products" -X GET -H "Authorization: Bearer $UAA_ACCESS_TOKEN" | jq '.[] | select(.installation_name=="p-bosh")' | jq -r .guid)
+BOSH_GUID=$(curl -k "https://pcf.${ENV_NAME}.cf-app.com/api/v0/staged/products" -X GET -H "Authorization: Bearer $UAA_ACCESS_TOKEN" | jq '.[] | select(.installation_name=="p-bosh")' | jq -r .guid)
 echo "BOSH_GUID: ${BOSH_GUID}"
 curl -v -k "https://pcf.${ENV_NAME}.cf-app.com/api/v0/staged/products/${BOSH_GUID}/networks_and_azs" \
     -X PUT \
