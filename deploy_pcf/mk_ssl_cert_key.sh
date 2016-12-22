5cb2a2da176d2e44cfa48fc31ff8570ce3a92c6f
Todd Ritchie <tritchie+github@pivotal.io>
2016-12-22 11:50:53 -0500

    Moves export domain cert and key out of script into pipeline. [#136377105]
    Signed-off-by: Derwei Chan <derwei@pivotal.io>

--- deploy_pcf/mk_ssl_cert_key.sh
+++ deploy_pcf/mk_ssl_cert_key.sh
@@ -10,7 +10,3 @@ openssl genrsa -out ${DOMAIN}.key 2048
 
 
 openssl req -x509 -newkey rsa:2048 -keyout ${DOMAIN}.key -out ${DOMAIN}.crt -days 3650 -nodes -subj "/C=US/ST=California/L=San Francisco/O=Pivotal Software Inc./CN=*.${DOMAIN}/emailAddress=cf-toolsmiths@pivotal.io/subjectAltName=DNS.1=${DOMAIN}"
-
-
-export DOMAIN_CRT=$(cat ${DOMAIN}.crt)
-export DOMAIN_KEY=$(cat ${DOMAIN}.key)
