5d8a1a711589d756c31dd1a0d49fb74b72e5530b
Todd Ritchie <tritchie+github@pivotal.io>
2016-12-22 11:44:43 -0500

    Adds ability to configure ERT with OM.
    [#136377105]
    
    Signed-off-by: Derwei Chan <derwei@pivotal.io>

---
+++ deploy_pcf/mk_ssl_cert_key.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -ex 
+
+DOMAIN=$1
+
+SSL_FILE=sslconf-${DOMAIN}.conf
+
+openssl genrsa -out ${DOMAIN}.key 2048
+
+
+openssl req -x509 -newkey rsa:2048 -keyout ${DOMAIN}.key -out ${DOMAIN}.crt -days 3650 -nodes -subj "/C=US/ST=California/L=San Francisco/O=Pivotal Software Inc./CN=*.${DOMAIN}/emailAddress=cf-toolsmiths@pivotal.io/subjectAltName=DNS.1=${DOMAIN}"
+
+
+export DOMAIN_CRT=$(cat ${DOMAIN}.crt)
+export DOMAIN_KEY=$(cat ${DOMAIN}.key)
