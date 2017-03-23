022d730f33a3c242661f3161acf26531156a413c
Mark Stokan <mstokan@pivotal.io>
2017-03-23 14:44:44 -0700

    Add additional wildcard SANs
    [#142282125]
    
    Signed-off-by: Joe Baldwin <jbaldwin@pivotal.io>

--- deploy_pcf/mk_ssl_cert_key.sh
+++ deploy_pcf/mk_ssl_cert_key.sh
@@ -4,9 +4,5 @@ set -ex
 
 DOMAIN=$1
 
-SSL_FILE=sslconf-${DOMAIN}.conf
 
-openssl genrsa -out ${DOMAIN}.key 2048
-
-
-openssl req -x509 -newkey rsa:2048 -keyout ${DOMAIN}.key -out ${DOMAIN}.crt -days 3650 -nodes -subj "/C=US/ST=California/L=San Francisco/O=Pivotal Software Inc./CN=*.${DOMAIN}/emailAddress=cf-toolsmiths@pivotal.io/subjectAltName=DNS.1=${DOMAIN}"
+openssl req -x509 -newkey rsa:2048 -keyout ${DOMAIN}.key -out ${DOMAIN}.crt -days 3650 -nodes -subj "/C=US/ST=California/L=San Francisco/O=Pivotal Software Inc./CN=*.${DOMAIN}/emailAddress=cf-toolsmiths@pivotal.io/subjectAltName=DNS.1=${DOMAIN},DNS.2=*.uaa.${DOMAIN},DNS.3=*.login.${DOMAIN}"
