503a795564607111fd392c51be2c4f129e764dc9
Navdeep Pama <npama@pivotal.io>
2017-11-16 11:34:37 -0500

    Allow a resource override file to be specified.
    [#152047234]
    
    Signed-off-by: Derwei Chan <dchan@pivotal.io>

--- deploy_pcf/generate_minimal_ert_resource_list.rb
+++ deploy_pcf/generate_minimal_ert_resource_list.rb
@@ -2,14 +2,19 @@
 
 require 'json'
 
-contents = File.read(ARGV[0])
-json = JSON.parse(contents)
+original_resources = File.read(ARGV[0])
+resource_overrides = ARGV[1].nil? ? '{}' : File.read(ARGV[1])
+
+original_resources_json = JSON.parse(original_resources)
 instances_to_modify = {}
-json['resources'].each do |resource|
+
+original_resources_json['resources'].each do |resource|
   if resource['instances_best_fit'] > 1
     instances_to_modify[resource['identifier']] = {instances: 1}
   end
 end
+
+instances_to_modify.merge!(JSON.parse(resource_overrides))
 
 File.open('modified_resources.json','w') do |f|
   f.write(instances_to_modify.to_json)
