c356ff49de555901156344c3a32452c6e705d72d
Ed Espino <edespino@pivotal.io>
2018-03-08 12:02:41 -0500

    Upgrade to terraforming-gcp v0.21
    Squashed commit of the following:
    
    commit 27b93bae2c2aa7fa0c8d17102d6625a070d354c5
    Author: Kenneth Lakin <klakin@pivotal.io>
    Date:   Tue Mar 6 12:16:21 2018 -0800
    
        Convert credhub-tfstate to Ruby
    
        This removes our dependence on uuencode/decode and makes working with
        the encoded data in our teardown job easier.
    
        [#155749612]
    
        Signed-off-by: Derwei Chan <dchan@pivotal.io>
    
    commit 76395567f065f5b7dcd3a7c058adab897a5b8167
    Author: Derwei Chan <dchan@pivotal.io>
    Date:   Tue Mar 6 11:41:42 2018 -0500
    
        Remove deprecated files.
    
    commit 8a4cea34c60356b23ea0829a86d07beb84c1594c
    Author: Ed Espino <edespino@pivotal.io>
    Date:   Mon Mar 5 16:55:51 2018 -0800
    
        Same change as the previous commmit. Different file
    
        Signed-off-by: Kenneth Lakin <klakin@pivotal.io>
    
    commit 8b4e63336d7a0da1519b854de7fb246e5169170f
    Author: Kenneth Lakin <klakin@pivotal.io>
    Date:   Mon Mar 5 16:19:25 2018 -0800
    
        terraforming-gcp v0.20 renamed ert_subnet to pas_subnet
    
        FIXME: If this works, make the same change to this file in the rest of
        the versioned directories.
    
    commit 42f974cabbadf53a411abdd441aa938c6a6c732a
    Author: Kenneth Lakin <klakin@pivotal.io>
    Date:   Mon Mar 5 14:32:13 2018 -0800
    
        Wrapper to compress+encode terraform.tfstate
    
        Currently uses uuencode and gzip to compress/uncompress.
        Stores data in $ENV_NAME/tfstate_gz_uu
        Set ENV_NAME env var and pass "set" or "get".
    
        Signed-off-by: Ed Espino <edespino@pivotal.io>
    
    commit 93c0ac257528cf8db2f5739c867795adf4964713
    Author: Navdeep Pama <npama@pivotal.io>
    Date:   Fri Mar 2 11:29:24 2018 -0500
    
        Terraform v0.11.3 requires the variable name change for
        ssl_cert_private_key
    
        [#155476328]
    
        Signed-off-by: Derwei Chan <dchan@pivotal.io>
    
    Signed-off-by: Derwei Chan <dchan@pivotal.io>

--- deprecated/aws/env.sh
+++
@@ -1,9 +0,0 @@
-export AWS_ACCESS_KEY_ID=access-key-id-bla
-export AWS_SECRET_ACCESS_KEY=secret-key-bla
-export AWS_ENVIRONMENT_NAME=toolsmithsaws
-export AWS_REGION=us-east-1
-export AWS_KEY_PAIR_NAME=toolsmiths-aws-kp
-export OPS_MANAGER_RDS_PASSWORD=blapasswordbla
-export OPS_MANAGER_RDS_USERNAME=blabla
-export VMS_SECURITY_GROUP_NAME=gupgup
-export AWS_PRIVATE_KEY_PATH=full-path-to-you-private-key
