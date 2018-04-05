928728bcb38f517613d37bb3ff1e77629ce82d62
Mark Stokan <mstokan@pivotal.io>
2018-04-05 11:09:01 -0700

    generate and pass along a root CA to terrform
    [#156525453]
    
    Signed-off-by: Kenneth Lakin <klakin@pivotal.io>

--- deploy_pcf/mk_ssl_cert_key.sh
+++ deploy_pcf/mk_ssl_cert_key.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 set -e
 
-[[ ${1} ]] && DOMAIN=${1}
-: ${DOMAIN:?must be set the DNS domain root (ex: example.cf-app.com)}
+[[ ${1} ]] && ENV_NAME=${1}
+: ${ENV_NAME:?must be set the environment name (ex: mammoth)}
 : ${KEY_BITS:=2048}
 : ${DAYS:=365}
 
-openssl req -new -x509 -nodes -sha256 -newkey rsa:${KEY_BITS} -days ${DAYS} -keyout ${DOMAIN}.key -out ${DOMAIN}.crt -config <( cat << EOF
+openssl req -new -x509 -nodes -sha256 -newkey rsa:${KEY_BITS} -days ${DAYS} -keyout ${ENV_NAME}.ca.key -out ${ENV_NAME}.ca.crt -config <( cat << EOF
 [ req ]
 prompt = no
 distinguished_name    = dn
-x509_extensions = alternate_names
 
 [ dn ]
 C  = US
 O = Pivotal
-CN = *.${DOMAIN}
 
-[ alternate_names ]
-keyUsage = critical, digitalSignature
-extendedKeyUsage = clientAuth, serverAuth
-subjectKeyIdentifier = hash
-authorityKeyIdentifier = keyid, issuer
-subjectAltName = DNS:*.${DOMAIN}, DNS:*.apps.${DOMAIN}, DNS:*.sys.${DOMAIN}, DNS:*.login.sys.${DOMAIN}, DNS:*.uaa.sys.${DOMAIN}
 EOF
 )
