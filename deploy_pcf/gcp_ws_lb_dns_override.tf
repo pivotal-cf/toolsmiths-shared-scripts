5282d70ddd919e4a8bb57df0da230b0399a4dec9
Kenneth Lakin <klakin@pivotal.io>
2018-06-01 16:16:22 -0700

    Use Websocket LBs rather than HTTP LBs for Router
    L4 "Websocket" LBs may be more reliable than L7 HTTP LBs. Let's see if
    this is true.
    
    Note: The Terraform Override file added here will need to be copied in
    to the Terraform working directory to take effect. This will require a
    deployment pipeline template change.
    
    [#157983400]

---
+++ deploy_pcf/gcp_ws_lb_dns_override.tf
@@ -0,0 +1,7 @@
+resource "google_dns_record_set" "wildcard-sys-dns" {
+  rrdatas = ["${google_compute_address.cf-ws.address}"]
+}
+
+resource "google_dns_record_set" "wildcard-apps-dns" {
+  rrdatas = ["${google_compute_address.cf-ws.address}"]
+}
