2c1456ff7c2de78ec72e138ee479986ddfd67734
Navdeep Pama <npama@pivotal.io>
2018-08-15 15:10:46 -0400

    Revert "Use Websocket LBs rather than HTTP LBs for Router"
    [#159644050]
    
    This reverts commit 5282d70ddd919e4a8bb57df0da230b0399a4dec9.

--- deploy_pcf/gcp_ws_lb_dns_override.tf
+++
@@ -1,7 +0,0 @@
-resource "google_dns_record_set" "wildcard-sys-dns" {
-  rrdatas = ["${google_compute_address.cf-ws.address}"]
-}
-
-resource "google_dns_record_set" "wildcard-apps-dns" {
-  rrdatas = ["${google_compute_address.cf-ws.address}"]
-}
