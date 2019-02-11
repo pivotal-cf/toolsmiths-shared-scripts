349ebdec173aef5a38609cfe991c41db15fc4ce7
Kenneth Lakin <klakin@pivotal.io>
2019-02-11 15:21:38 -0800

    Corrects 'desired resource' determination logic
    I should have written this correctly the first time.
    
    [#163839863]

--- deploy_pcf/generate_minimal_ert_resource_list.rb
+++ deploy_pcf/generate_minimal_ert_resource_list.rb
@@ -9,17 +9,19 @@ original_resources_json = JSON.parse(original_resources)
 instances_to_modify = {}
 
 original_resources_json['resources'].each do |resource|
+  desired_resources = 0
   #"" means "Use the 'Automatic' value, stored in instances_best_fit"
   if resource['instances'] == ""
-    if resource['instances_best_fit'] <= 1
-      next
+    if resource['instances_best_fit'] == ""
+      puts "WARN: instances and instances_best_fit are both the empty string. This is unexpected and unhandled."
     end
+    desired_resources = resource['instances_best_fit']
+  else
+    desired_resources = resource['instances']
   end
-  if resource['instances'] <= 1
-    next
+  if desired_resources > 1
+    instances_to_modify[resource['identifier']] = {instances: 1}
   end
-
-  instances_to_modify[resource['identifier']] = {instances: 1}
 end
 
 instances_to_modify.merge!(JSON.parse(resource_overrides))
