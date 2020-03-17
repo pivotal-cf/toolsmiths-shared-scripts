9fef7cab927bb9ec9137ce8e9abb9bb88c245d0e
Mike Jarvis <mjarvis@pivotal.io>
2020-03-17 09:40:12 -0700

    explicitly change SSL expiration days
    need to change this back, too.

--- deploy_pcf/mk_ssl_cert_key.sh
+++ deploy_pcf/mk_ssl_cert_key.sh
@@ -4,7 +4,7 @@ set -e
 [[ ${1} ]] && DOMAIN=${1}
 : ${DOMAIN:?must be set the DNS domain root (ex: example.cf-app.com)}
 : ${KEY_BITS:=2048}
-: ${DAYS:=1}
+: ${DAYS:=365}
 
 openssl req -new -x509 -nodes -sha256 -newkey rsa:${KEY_BITS} -days ${DAYS} -keyout ${DOMAIN}.ca.key.pkcs8 -out ${DOMAIN}.ca.crt -config <( cat << EOF
 [ req ]
@@ -37,7 +37,7 @@ subjectAltName = DNS:*.${DOMAIN}, DNS:*.apps.${DOMAIN}, DNS:*.sys.${DOMAIN}, DNS
 EOF
 )
 
-openssl x509 -req -in ${DOMAIN}.csr -CA ${DOMAIN}.ca.crt -CAkey ${DOMAIN}.ca.key.pkcs8 -CAcreateserial -out ${DOMAIN}.host.crt -days ${DAYS} -sha256 -extfile <( cat << EOF
+openssl x509 -req -in ${DOMAIN}.csr -CA ${DOMAIN}.ca.crt -CAkey ${DOMAIN}.ca.key.pkcs8 -CAcreateserial -out ${DOMAIN}.host.crt -days 1 -sha256 -extfile <( cat << EOF
 basicConstraints = CA:FALSE
 subjectAltName = DNS:*.${DOMAIN}, DNS:*.apps.${DOMAIN}, DNS:*.sys.${DOMAIN}, DNS:*.login.sys.${DOMAIN}, DNS:*.uaa.sys.${DOMAIN}
 subjectKeyIdentifier = hash
