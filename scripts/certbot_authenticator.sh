be84f1cbcd0264278d4a481b66140b2c858d0d2d
Bin Ju <bju@pivotal.io>
2020-03-23 17:00:23 -0400

    add scripts for certbot
    [#171890378]
    
    Signed-off-by: Mike Jarvis <mjarvis@pivotal.io>

---
+++ scripts/certbot_authenticator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# authenticate to the GCP Project
+echo $GCP_SERVICE_ACCOUNT_KEY > account_key.json
+gcloud auth activate-service-account --key-file account_key.json
+
+# Create TXT record
+gcloud dns record-sets transaction start --zone=${ENV_NAME}-zone
+gcloud dns record-sets transaction add "$CERTBOT_VALIDATION" --name=_acme-challenge.pcf.${ENV_NAME}.cf-app.com. --ttl=300 --type=TXT --zone=${ENV_NAME}-zone
+gcloud dns record-sets transaction execute --zone=${ENV_NAME}-zone
+
+# Sleep to make sure the change has time to propagate over to DNS
+sleep 300
---
+++ scripts/certbot_cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "the cleanup script was run"
