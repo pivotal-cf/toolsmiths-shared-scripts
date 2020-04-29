03b28fd4cf131a905ccf7509c2cd22f2dcdf3517
Mike Jarvis <mjarvis@pivotal.io>
2020-04-29 16:07:02 -0700

    Update nimbus_dual_networks.rb

--- scripts/nimbus_dual_networks.rb
+++ scripts/nimbus_dual_networks.rb
@@ -1,20 +1,23 @@
 {
-  'name' => 'ntf-testbed-multi-networks',
-  'network' => [
-    {
-      'name' => 'net.0'
-    }
-  ],
-  'genericVM' => [
-    {
-      'type' => 'worker-template',
-      'nics' => 2,
-      'networks' => ['public', 'nsx::net.0']
-    },
-    {
-      'type' => 'worker-template',
-      'nics' => 2,
-      'networks' => ['nsx::net.0']
-    }
-  ]
+"network" => [
+   {
+     "name" => "net.0",
+     "enableDhcp" => true
+   }
+],
+ 
+genericVM => [
+   {
+      "name" => "worker.0",
+      "type" => "worker-template",
+      "nics" => 2,
+      "networks" => ["public", "nsx::net.0"]
+   },
+   {
+      "name" => "worker.1",
+      "type" => "worker-template",
+      "nics" => 2,
+      "networks" => ["nsx::net.0"]
+   }
+]
 }
