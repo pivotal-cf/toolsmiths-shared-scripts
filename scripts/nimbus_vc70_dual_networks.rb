823d72141364ec72ef50b16eff5b1f6f211dbc90
Mike Jarvis <mjarvis@pivotal.io>
2020-05-03 13:16:56 -0700

    enable static IP service on network
    Also, vcenter only needs 1 NIC

--- scripts/nimbus_vc70_dual_networks.rb
+++ scripts/nimbus_vc70_dual_networks.rb
@@ -1,18 +1,21 @@
 oneGB = 1 * 1000 * 1000 # in KB
-
+ 
 $testbed = Proc.new do
   {
     "name" => "testbed-test",
     "version" => 3,
-     "network" => [
-      {
-        "name" => "net.0",
-        "enableDhcp" => true
-      }],
-
+    "network" => [
+        {
+          "name" => "net.0",
+          "enableDhcp" => true,
+          "enableStaticIpService" => true
+        }
+                  ],
     "esx" => (0..0).map do | idx |
       {
         "name" => "esx.#{idx}",
+        "nics" => 2, # 2 NICs
+        "networks" => ["public", "nsx::net.0"],
         "vc" => "vc.0",
         "customBuild" => "ob-15843807",
         "dc" => "vcqaDC",
@@ -21,12 +24,16 @@ $testbed = Proc.new do
         "cpus" => 32, # 32 vCPUs
         "memory" => 98000, # 98GB memory
         "fullClone" => true,
-        "disks" => [ 10 * 1000 * oneGB ], # 10 TB Diska
-        "nics" => 2,
-        "networks" => ["public", "nsx::net.0"]
+        "disks" => [ 10 * 1000 * oneGB ], # 10 TB Disk
+        "guestOSlist" => [         
+          {
+            "vmName" => "centos-vm.#{idx}",
+            "ovfuri" => NimbusUtils.get_absolute_ovf("CentOS6_x64_2GB/CentOS6_x64_2GB.ovf")
+          }
+        ]
       }
     end,
-
+ 
     "vcs" => [
       {
         "name" => "vc.0",
@@ -39,12 +46,10 @@ $testbed = Proc.new do
             "name" => "cluster0",
             "dc" => "vcqaDC"
           }
-        ],
-        "nics" => 2,
-        "networks" => ["public", "nsx::net.0"]
+        ]
       }
     ],
-
+ 
     "beforePostBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
     end,
     "postBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
