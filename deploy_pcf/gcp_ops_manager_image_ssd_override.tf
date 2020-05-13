35a8f97105a1272f5653559c9a0a38e3c3b6259b
Navdeep Pama <navdeeppama@Navdeep-Pama-8635.local>
2020-05-13 10:08:41 -0400

    Revert "Use ssd image type for opsman"
    This reverts commit 4e9237f055193fde1fc5074718c0378bc0ac65c1.

--- deploy_pcf/gcp_ops_manager_image_ssd_override.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_image" "ops-manager-image" {
-  boot_disk {
-    initialize_params {
-      image = "${google_compute_image.ops-manager-image.self_link}"
-      type  = "pd-ssd"
-      size  = 150
-    }
-  }
-}
