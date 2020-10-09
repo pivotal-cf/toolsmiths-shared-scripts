29b2ac5cd03cb53c429371132564f648113c0bf8
Tom Viehman <tviehman@pivotal.io>
2020-10-09 11:04:30 -0700

    Print true/false instead of exiting 1/0 in has_custom_vm_types
    Deploy pipeline failed because it had set the `-e` flag in the task that
    invokes the has_custom_vm_types script, so the task would fail if the
    custom_vm_types weren't already present.

--- deploy_pcf/has_custom_vm_types.rb
+++ deploy_pcf/has_custom_vm_types.rb
@@ -6,4 +6,9 @@ default_vm_types = JSON.parse(File.read(ARGV[0]))['vm_types']
 custom_vm_types = JSON.parse(File.read(ARGV[1]))
 
 shared_vm_types = default_vm_types & custom_vm_types
-exit 1 if shared_vm_types != custom_vm_types
+if shared_vm_types != custom_vm_types
+  puts false
+  exit(0)
+end
+
+puts true
