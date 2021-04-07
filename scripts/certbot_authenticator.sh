e95237e72cdab3c43b5943ab30af26437c7b0304
Bin Ju <bju@pivotal.io>
2021-04-07 17:04:01 -0400

    Squashed commit of the following:
    commit 646ede476e4011ff0a9d45d285266ff92fe532b8
    Author: Bin Ju <bju@pivotal.io>
    Date:   Tue Apr 6 22:19:24 2021 -0400
    
        Make the authenticator work for multiple domains
    
        [#177566499]
    
        Signed-off-by: Bin Ju <bju@pivotal.io>
    
    commit e599345a1043425196e87ea478eabaf669b617b6
    Author: Bin Ju <bju@pivotal.io>
    Date:   Mon Apr 5 18:07:54 2021 -0400
    
        Use the cert that `letsencrypt` generated for *.apps, *.sys, and so on...
    
        Signed-off-by: Bin Ju <bju@pivotal.io>
    
    Signed-off-by: Bin Ju <bju@pivotal.io>

--- scripts/certbot_authenticator.sh
+++ scripts/certbot_authenticator.sh
@@ -6,8 +6,18 @@ gcloud auth activate-service-account --key-file account_key.json
 
 # Create TXT record
 gcloud dns record-sets transaction start --zone=${ENV_NAME}-zone
-gcloud dns record-sets transaction add "$CERTBOT_VALIDATION" --name=_acme-challenge.pcf.${ENV_NAME}.cf-app.com. --ttl=300 --type=TXT --zone=${ENV_NAME}-zone
+challenge_record=$(gcloud dns record-sets list -z ${ENV_NAME}-zone | grep "_acme-challenge.${CERTBOT_DOMAIN}.")
+if [ -n "$challenge_record" ]; then
+	echo "found the TXT record: ${challenge_record}"
+	r_name=$(echo $challenge_record | awk '{print $1}')
+	r_type=$(echo $challenge_record | awk '{print $2}')
+	r_ttl=$(echo $challenge_record | awk '{print $3}')
+	r_data=$(echo $challenge_record | awk '{print $4}')
+	gcloud dns record-sets transaction remove ${r_data} --name=${r_name} --type=${r_type} --ttl=${r_ttl} --zone=${ENV_NAME}-zone
+fi
+
+gcloud dns record-sets transaction add "$CERTBOT_VALIDATION" --name=_acme-challenge.${CERTBOT_DOMAIN}. --ttl=300 --type=TXT --zone=${ENV_NAME}-zone
 gcloud dns record-sets transaction execute --zone=${ENV_NAME}-zone
 
 # Sleep to make sure the change has time to propagate over to DNS
-sleep 300
+sleep 120
